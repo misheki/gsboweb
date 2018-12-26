6c69b49d7a4428f2f60378a67afc08a3a15601dd
alin <halimatun@one2pay.my>
2018-12-26 12:01:52 +0800

    add UI import stock & view stock

--- src/components/stock_management/ImportStock.js
+++ src/components/stock_management/ImportStock.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
-import { Layout,  Table, Modal  } from 'antd';
-
+import { Layout,  Upload, Icon, message  } from 'antd';
 
 const { Header } = Layout;
+const Dragger = Upload.Dragger;
+
+const props = {
+    name: 'file',
+    multiple: true,
+    action: '//jsonplaceholder.typicode.com/posts/',
+    onChange(info) {
+      const status = info.file.status;
+      if (status !== 'uploading') {
+        console.log(info.file, info.fileList);
+      }
+      if (status === 'done') {
+        message.success(`${info.file.name} file uploaded successfully.`);
+      } else if (status === 'error') {
+        message.error(`${info.file.name} file upload failed.`);
+      }
+    },
+};
 
 class ImportStock extends Component {
 
@@ -20,7 +37,13 @@ class ImportStock extends Component {
             <Header style={{ color: 'white', fontSize: '30px' }}>
                 <span>Import Stock</span>
             </Header>
-           
+            <Dragger {...props}>
+                <p className="ant-upload-drag-icon">
+                <Icon type="inbox" />
+                </p>
+                <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                <p className="ant-upload-hint">Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files</p>
+            </Dragger>,
             </div>
             );   
         }
--- src/components/stock_management/ViewStock.js
+++ src/components/stock_management/ViewStock.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Layout,  Table, Modal  } from 'antd';
+import { Layout,  Table  } from 'antd';
 
 
 const { Header } = Layout;
+const { Column } = Table;
 
 class ViewStock extends Component {
 
@@ -20,6 +21,19 @@ class ViewStock extends Component {
             <Header style={{ color: 'white', fontSize: '30px' }}>
                 <span>View Stock</span>
             </Header>
+            <div style={{ padding: '30px' }}>
+                 <Table
+                    // dataSource={packages}
+                    // rowKey={pakages => packages.id}
+                    >
+                    <Column title="Package ID" dataIndex="package_id" key="package_id" />
+                    <Column title="Sim Card Number" dataIndex="sim_card_number" key="sim_card_number" />
+                    <Column title="Variation ID" dataIndex="variation_id" key="variation_id" />
+                    <Column title="Serial Number" dataIndex="serial_number" key="serial_number" />
+                    <Column title="Stock Status" dataIndex="status" key="status" />
+                    <Column title="Action" dataIndex="action" key="action" />
+                </Table>
+            </div>
            
             </div>
             );   
