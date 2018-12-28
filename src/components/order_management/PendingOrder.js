8f361c3550596441f0faa0b1f3380834999a231f
alin <halimatun@one2pay.my>
2018-12-28 18:13:21 +0800

    UI change

--- src/components/order_management/PendingOrder.js
+++ src/components/order_management/PendingOrder.js
@@ -1,106 +1,18 @@
 import React, { Component } from 'react';
-import { Layout,  Table, Steps, Button, message, Form, Input, Select  } from 'antd';
+import { Layout,  Table, Steps, Button, message, Form, Input, Select, Col, Row, Divider   } from 'antd';
 
 const Option = Select.Option;
 const Step = Steps.Step;
 const { Header } = Layout;
 const { Column } = Table;
 const formItemLayout = {
-    labelCol: { span: 4 },
+    labelCol: { span: 12 },
     wrapperCol: { span: 12 },
   };
 const formTailLayout = {
 labelCol: { span: 4 },
 wrapperCol: { span: 8, offset: 6 },
 };
-
-const steps = [{
-  title: 'Customer Details',
-  content: <Form> 
-                <Form.Item  {...formItemLayout} label="Name">
-                    <Input name="name" />
-                </Form.Item>
-                <Form.Item  {...formItemLayout} label="Contact Number">
-                    <Input name="contact_num" />
-                </Form.Item>
-                <Form.Item  {...formItemLayout} label="Email">
-                    <Input name="email" />
-                </Form.Item>
-                <Form.Item  {...formItemLayout} label="Shipping Address">
-                    <Input name="address" />
-                </Form.Item>
-                <Form.Item  {...formItemLayout} label="Postcode">
-                    <Input name="postcode" />
-                </Form.Item>
-            </Form>
-}, {
-  title: 'Order Details',
-  content:
-  <div>
-    <Form> 
-        <Form.Item  {...formItemLayout} label="Package ID">
-            <Input name="name" />
-        </Form.Item>
-        <Form.Item  {...formItemLayout} label="Package name">
-            <Input name="contact_num" />
-        </Form.Item>
-        <Form.Item  {...formItemLayout} label="Quantity">
-            <Input name="email" />
-        </Form.Item>
-        <Form.Item  {...formItemLayout} label="Unit Prize">
-            <Input name="address" />
-        </Form.Item>
-        <Form.Item  {...formItemLayout} label="Total Prize">
-            <Input name="postcode" />
-        </Form.Item>
-    </Form>
-    <Form.Item {...formTailLayout}>
-        <Button type="primary">
-            Request Stock
-        </Button>
-    </Form.Item>  
-  </div>
-  
-}, {
-  title: 'Shipping Details',
-  content:  
-  <div>
-    <Form> 
-        <Form.Item  {...formItemLayout} label="Courier">
-            <Select
-                showSearch
-                placeholder="Please select courier"
-                optionFilterProp="children"
-                filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-            >
-                <Option value="jack">Jack</Option>
-                <Option value="lucy">Lucy</Option>
-                <Option value="tom">Tom</Option>
-            </Select>
-        </Form.Item>
-        <Form.Item  {...formItemLayout} label="Recipient Name">
-            <Input name="name" />
-        </Form.Item>
-        <Form.Item  {...formItemLayout} label="Shipping Address">
-            <Input name="address" />
-        </Form.Item>
-        <Form.Item  {...formItemLayout} label="Contact Number">
-            <Input name="contact_num" />
-        </Form.Item>
-        <Form.Item  {...formItemLayout} label="Email">
-            <Input name="email" />
-        </Form.Item>
-        <Form.Item  {...formItemLayout} label="Tracking Number">
-            <Input name="tracking" />
-        </Form.Item>
-    </Form>
-</div>
-},
-{
-    title: 'Confirm Order',
-    content: 'Confirm Package',
-}];
-
 
 
 class PendingOrder extends Component {
@@ -124,6 +36,157 @@ class PendingOrder extends Component {
 
     render() {
         const { current } = this.state;
+        const { getFieldDecorator } = this.props.form;
+
+        const steps = [{
+            title: 'Order Details',
+            content:  
+            <Form layout="vertical" style={{width:'90%', paddingLeft:'40px',}}>
+             <Divider orientation="left">Customer Details</Divider>
+                <Row style={{paddingTop:'20px'}} gutter={8}>
+                    <Col span={8}>
+                        <Form.Item label="Order Number">
+                            {getFieldDecorator('order_number', {
+                                rules: [{ required: true, message: '' }]
+                            })(
+                                <Input disabled />
+                            )}
+                        </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                        <Form.Item label="Order Date">
+                            {getFieldDecorator('order_number', {
+                                rules: [{ required: true, message: '' }]
+                            })(
+                                <Input disabled/>
+                            )}
+                        </Form.Item>
+                    </Col>
+                    <Col span={8}>
+                        <Form.Item label="Sale Channel">
+                            {getFieldDecorator('order_number', {
+                                rules: [{ required: true, message: '' }]
+                            })(
+                                <Input disabled/>
+                            )}
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Form.Item  label="Customer Name">
+                    {getFieldDecorator('order_number', {
+                        rules: [{ required: true, message: '' }]
+                    })(
+                        <Input name="order_number" />
+                    )}
+                </Form.Item>
+             
+                <Row gutter={8}>
+                    <Col span={12}>
+                        <Form.Item label="Email">
+                            {getFieldDecorator('email', {
+                                rules: [{ 
+                                type: 'email', message: 'The input is not valid E-mail!',
+                                required: true, message: '' }]
+                            })(
+                                <Input name="email" />
+                            )}
+                        </Form.Item> 
+                    </Col>
+                    <Col span={12}>
+                        <Form.Item label="Contact Number">
+                            {getFieldDecorator('contact_num', {
+                                rules: [{ required: true, message: '' }]
+                            })(
+                                <Input name="contact_num" />
+                            )}
+                        </Form.Item>  
+                    </Col>
+                </Row>
+                <Divider orientation="left">Package Details</Divider>
+                <Row  style={{paddingTop:'20px'}} gutter={16}>
+                    <Col span={2}>
+                        <Form.Item   label="No">
+                            <Input className="input-field"  disabled  />
+                            <Input disabled  />
+                        </Form.Item>
+                    </Col>
+                    <Col span={4}>
+                        <Form.Item   label="SKU">
+                            <Input className="input-field"  disabled  />
+                            <Input disabled  />
+                        </Form.Item>
+                    </Col>
+                    <Col span={6}>
+                        <Form.Item   label="Package">
+                            <Input className="input-field"  disabled  />
+                            <Input disabled  />
+                        </Form.Item>
+                    </Col>
+                    <Col span={6}>
+                        <Form.Item   label="Sim Card Number">
+                            <Input className="input-field"  />
+                            <Input  />
+                        </Form.Item>
+                    </Col>
+                    <Col span={6}>
+                        <Form.Item   label="Serial Number">
+                            <Input className="input-field"  />
+                            <Input  />
+                        </Form.Item>
+                    </Col>
+                </Row>
+          </Form>
+          }, {
+            title: 'Shipping Details',
+            content:  
+              <Form  layout="vertical" style={{width:'90%', paddingLeft:'40px',}}> 
+                  <Form.Item label="Courier">
+                      <Select
+                          showSearch
+                          placeholder="Please select courier"
+                          optionFilterProp="children"
+                          filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                      >
+                          <Option value="jack">Gdex</Option>
+                          <Option value="lucy">Pos Laju</Option>
+                          <Option value="tom">AirPax</Option>
+                      </Select>
+                  </Form.Item>
+                  <Form.Item  label="Shipping Adddress">
+                            {getFieldDecorator('order_number', {
+                                rules: [{ required: true, message: '' }]
+                            })(
+                                <Input name="order_number" />
+                            )}
+                        </Form.Item>
+                        <Row gutter={8}>
+                            <Col span={14}>
+                                <Form.Item label='State'>
+                                    {getFieldDecorator('website', {
+                                    
+                                    })(
+                                        <Select />
+                                    )}
+                                </Form.Item>
+                            </Col>
+                            <Col span={10}>
+                                <Form.Item label='Postcode'>
+                                    {getFieldDecorator('status', {
+                                        
+                                    })(
+                                        <Input />
+                                        
+                                    )}
+                                </Form.Item>
+                            </Col>
+                    </Row>
+              </Form>
+          },
+          {
+              title: 'Confirm Order',
+              content: 'Confirm Package',
+          }];
+          
          return (
             <div>
             <Header style={{ color: 'white', fontSize: '30px' }}>
@@ -135,9 +198,8 @@ class PendingOrder extends Component {
                     // rowKey={pakages => packages.id}
                     >
                     <Column title="Order Number" dataIndex="order_number" key="order_number" />
-                    <Column title="Product Items" dataIndex="items" key="items" />
-                    <Column title="Quantity" dataIndex="quantity" key="quantity" />
-                    <Column title="Unit Prize" dataIndex="unit_prize" key="unit_prize" />
+                    <Column title="Order Date" dataIndex="items" key="items" />
+                    <Column title="Customer Name" dataIndex="items" key="items" />
                     <Column title="Total" dataIndex="total" key="total" />
                     <Column title="Order Status" dataIndex="status" key="status" />
                     <Column title="Action" dataIndex="action" key="action" />
@@ -173,5 +235,5 @@ class PendingOrder extends Component {
        
 }
 
-export default PendingOrder;
+export default Form.create()( PendingOrder);
 
