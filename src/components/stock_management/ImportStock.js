1ec9b23972243246003a38ace1f4527b4187105a
Michelle Yee <michelle.yee@one2pay.my>
2018-12-28 18:40:21 +0800

    added helper Upload Stock

--- src/components/stock_management/ImportStock.js
+++ src/components/stock_management/ImportStock.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Layout,  Upload, Icon, message  } from 'antd';
+import { importStocks } from '../../helpers/Upload';
 
 const { Header } = Layout;
 const Dragger = Upload.Dragger;
@@ -9,11 +10,11 @@ const props = {
     multiple: true,
     action: global.URL + 'stock/create',
     onChange(info) {
+        console.log(info);
       const status = info.file.status;
       if (status !== 'uploading') {
-        console.log(info.file, info.fileList);
-      }
-      if (status === 'done') {
+        console.log(info.file, info.fileList); }
+      else if (status === 'done') {
         message.success(`${info.file.name} file uploaded successfully.`);
       } else if (status === 'error') {
         message.error(`${info.file.name} file upload failed.`);
@@ -26,11 +27,50 @@ class ImportStock extends Component {
     constructor(props) {
         super(props);
         this.state = {
-           
+            stocksfile : ''
         };
+
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.importStock = this.importStock.bind(this);
     }
 
+    importStock(f) {
+        var access_token = sessionStorage.getItem('access_token');
+        console.log(access_token);
+        importStocks(f, access_token)
+            .then(result => {
+                if (result.result === 'GOOD') {
+                    
+                }
+                console.log(result);
+            })
+    }
 
+    onFormSubmit(e){
+        e.preventDefault() 
+        this.importStock(this.state.stocksfile);
+    }
+
+    onChange(e) {
+        let files = e.target.files || e.dataTransfer.files;
+        if (!files.length)
+                return;
+
+        console.log(files[0]);
+        this.createImage(files[0]);
+    }
+
+    createImage(file) {
+        let reader = new FileReader();
+        reader.onload = (e) => {
+            this.setState({
+                stocksfile: e.target.result
+            })
+        };
+        reader.readAsDataURL(file);
+    }
+    
     render() {
          return (
             <div>
@@ -43,7 +83,12 @@ class ImportStock extends Component {
                 </p>
                 <p className="ant-upload-text">Click or drag file to this area to upload</p>
                 <p className="ant-upload-hint">Support for a single or bulk upload. Strictly prohibit from uploading company data or other band files</p>
-            </Dragger>,
+            </Dragger>
+            <br /><br />
+            <form onSubmit={this.onFormSubmit}>
+                <input type="file" onChange={this.onChange} />
+                <button type="submit">Upload</button>
+            </form>
             </div>
             );   
         }
