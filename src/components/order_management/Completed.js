9f0fee122fe2c666994fae50026609e0de91a7d6
alin <halimatun@one2pay.my>
2019-01-03 18:33:26 +0800

    edit completed order layout

--- src/components/order_management/Completed.js
+++ src/components/order_management/Completed.js
@@ -50,142 +50,118 @@ class Completed extends Component {
 
     renderDetails() {
         this.state.order.order_details.map((item) => console.log(item));
+
+        const formItemLayout = {
+            labelCol: {span:8},
+            wrapperCol: { span: 10 },
+    
+          };
+
+        const packageDetailItems =  this.state.order.order_details.map((item) =>
+        item.stocks.map((stock) =>
+            <React.Fragment key={item.id}>
+                <Row gutter={8}>
+                    <Col span={2}>
+                        <Form.Item>
+                            <Input value={stock.id} disabled />
+                        </Form.Item>
+                    </Col>
+                    <Col span={3}>
+                        <Form.Item>
+                            <Input value={item.sku} disabled />
+                        </Form.Item>
+                    </Col>
+                    <Col span={7}>
+                        <Form.Item>
+                            <Input value={item.package} disabled />
+                        </Form.Item>
+                    </Col>
+                    
+                    <Col span={6}>
+                        <Form.Item>
+                            <Input value={stock.serial_number} disabled />
+                        </Form.Item>
+                    </Col>
+                    <Col span={3}>
+                        <Form.Item>
+                            <Input  value={stock.sim_card_number} disabled />
+                        </Form.Item>
+                    </Col>
+                    <Col span={2}>
+                        <Form.Item>
+                            <Input  value={item.unit_price} disabled />
+                        </Form.Item>
+                    </Col>
+                </Row>
+            </React.Fragment>    
+            )
+        )
         return(
-            <div style={{padding:'30px', width:'90%'}}>
-                <Form layout="vertical">
-                    <div style={{backgroundColor:'white', padding:'20px', marginBottom:'10px'}}>
-                        <h2 style={{paddingBottom:'10px'}}>Order Ref. No. {this.state.order.order_ref_num}</h2>
-                        <Row gutter={16}>
-                            <Col span={8}>
-                                <FormItem label="Order Date">
-                                    <Input value={this.state.order.order_date} disabled/>
+            <div style={{padding:'30px'}}>
+                <Form layout="vertical" style={{backgroundColor:'white'}}> 
+                    <div style={{padding:'20px', marginBottom:'10px'}}>
+                        <h2 style={{paddingBottom:'10px'}}>Order Ref. No. {this.state.order.order_ref_num}</h2>    
+                        <Row gutter={8}>
+                            <Col span={12}>
+                            <h3 style={{paddingBottom:'10px'}}>Order Details </h3>  
+                                <FormItem {...formItemLayout} label="Order Date : "  className="form-item">
+                                <p>{this.state.order.order_date}</p> 
                                 </FormItem>
-                            </Col>
-                            <Col span={8}>
-                                <FormItem label="Total Amount">
-                                    <Input value={'RM ' + this.state.order.total} disabled/>
+                                <FormItem  {...formItemLayout} label="Sales Channel : " className="form-item">
+                                    <p>{this.state.order.sales_channel} </p>
                                 </FormItem>
-                            </Col>
-                            <Col span={8}>
-                                <FormItem label="Sales Channel">
-                                    <Input value={this.state.order.sales_channel} disabled/>
+                                <FormItem  {...formItemLayout} label="Shipping Method : " className="form-item">
+                                    <p>{this.state.order.shipping_method} </p>
                                 </FormItem>
+                                <FormItem  {...formItemLayout} label="Tracking Number : " className="form-item">
+                                    <p>{this.state.order.tracking_number} </p>
+                                </FormItem>
+
                             </Col>
-                        </Row>
-                        <h3 style={{paddingBottom:'10px'}}>Items</h3>
-                        <Row style={{backgroundColor: '#d2d2d2', padding: '1% 2% 0 2%'}}>
-                                <h4>Subtotal: RM {this.state.order.order_total}</h4>
-                            {this.state.order.order_details.map((item) =>
-                            <Row key={item.id} gutter={16}>
-                                <Row gutter={16}>
-                                    <Col span={6}>
-                                        <Form.Item label="SKU">
-                                            <Input value={item.sku} disabled />
-                                        </Form.Item>
-                                    </Col>
-                                    <Col span={10}>
-                                        <Form.Item label="Package">
-                                            <Input value={item.package} disabled />
-                                        </Form.Item>
-                                    </Col>
-                                    <Col span={4}>
-                                        <Form.Item label="Quantity">
-                                            <Input value={item.quantity} disabled />
-                                        </Form.Item>
-                                    </Col>
-                                    <Col span={4}>
-                                        <Form.Item label="Unit Price">
-                                            <Input value={item.unit_price} disabled />
-                                        </Form.Item>
-                                    </Col>
-                                </Row>
-                                {item.stocks.map((stock) =>
-                                    <Row gutter={16}>
-                                        <Col span={2} style={{textAlign: 'right'}} >
-                                            <Form.Item>
-                                            <Icon type="check-circle" theme="twoTone" twoToneColor="#52c41a" style={{ fontSize: '30pt',display: 'inline-block', verticalAlign: 'middle', lineHeight: '2.1em' }} />
-                                            </Form.Item>
-                                        </Col>
-                                        <Col span={6}>
-                                            <Form.Item label="Serial Number">
-                                                <Input value={stock.serial_number} disabled />
-                                            </Form.Item>
-                                        </Col>
-                                        <Col span={6}>
-                                            <Form.Item label="Sim Card Number">
-                                                <Input value={stock.sim_card_number} disabled />
-                                            </Form.Item>
-                                        </Col>
-                                        <Col span={10}>
-                                            <Form.Item>
-                                                
-                                            </Form.Item>                                           
-                                        </Col>
-                                    </Row>
-                                )}
+                            <Col span={12}>
+                                <h3 style={{paddingBottom:'10px'}}>Customer Details </h3>  
+                                <p>{this.state.order.customer_name}</p> 
+                                <p>{this.state.order.customer_address}</p> 
+                                <p>{this.state.order.customer_postcode}, {this.state.order.customer_state}</p> 
+                                <p>{this.state.order.customer_contact_num}</p> 
+                                <p>{this.state.order.customer_email}</p> 
+                            </Col>
+                        </Row>   
+                        <h3 style={{paddingBottom:'10px'}}>Product Details</h3>
+                        <div style={{ backgroundColor: 'white', padding:'10px'}}>
+                            <Row gutter={16} style={{ backgroundColor: '#e8e8e8', padding: '10px', paddingBottom: '0px', marginBottom: '10px' }}>
+                                <Col span={2}>
+                                    <p>Item</p>
+                                </Col>
+                                <Col span={3}>
+                                    <p>SKU</p>
+                                </Col>
+                                <Col span={7}>
+                                    <p>Package</p>
+                                </Col>
+                                <Col span={6}>
+                                    <p>Sim Card Number</p>
+                                </Col>
+                                <Col span={3}>
+                                    <p>Serial Number</p>
+                                </Col>
+                                <Col span={2}>
+                                    <p>Unit Price</p>
+                                </Col>
                             </Row>
-                        )}
-                        </Row>
-                        <h3 style={{paddingBottom:'10px', paddingTop:'50px'}}>Customer Information</h3>
-                        <Row gutter={16}>
-                            <Col span={8}>
-                                <FormItem  label="Customer Name">
-                                    <Input value={this.state.order.customer_name} disabled/>
+                            {packageDetailItems}
+                             <div style={{float:'right', width:'30%'}}>
+                                <FormItem  labelCol={{ span: 12 }} wrapperCol={{ span: 12 }} label="Subtotal : "  className="form-item">
+                                        <p>RM {this.state.order.order_total}</p> 
                                 </FormItem>
-                            </Col>
-                            <Col span={8}>
-                                <FormItem label="Email">
-                                    <Input value={this.state.order.customer_email} disabled/>
-                                </FormItem> 
-                            </Col> 
-                            <Col span={8}>
-                                <FormItem label="Contact Number">
-                                    <Input value={this.state.order.customer_contact_num} disabled/>
-                                </FormItem>  
-                            </Col>    
-                        </Row>
-                        <FormItem  label="Shipping Address">
-                            <Input value={this.state.order.customer_address} disabled/>
-                        </FormItem>
-                        <Row gutter={16}>
-                            <Col span={8}>
-                                <FormItem label='State'>
-                                    <Input value={this.state.order.customer_state} disabled/>
+                                <FormItem  labelCol={{ span: 12 }} wrapperCol={{ span: 12 }} label="Shipping Fee : "  className="form-item">
+                                        <p>RM {this.state.order.shipping_fee} </p> 
                                 </FormItem>
-                            </Col>
-                            <Col span={8}>
-                                <FormItem label='Postcode'>
-                                    <Input value={this.state.order.customer_postcode} disabled/>
+                                <FormItem  labelCol={{ span: 12 }} wrapperCol={{ span: 12 }} label="Total Amount : "  className="form-item">
+                                        <p>RM {this.state.order.total}</p> 
                                 </FormItem>
-                            </Col>
-                            <Col span={8}>
-                                <FormItem label="Contact Number">
-                                    <Input value={this.state.order.customer_contact_num} disabled/>
-                                </FormItem>  
-                            </Col>
-                        </Row>
-                        <Row gutter={16}>
-                            <Col span={6}>
-                                <FormItem label='Shipping Method'>
-                                    <Input value={this.state.order.shipping_method} disabled/>
-                                </FormItem>
-                            </Col>
-                            <Col span={6}>
-                                <FormItem label='Shipping Date'>
-                                    <Input value={this.state.order.shipped_at} disabled/>
-                                </FormItem>
-                            </Col>
-                            <Col span={6}>
-                                <FormItem label="Shipping Fee">
-                                    <Input value={this.state.order.shipping_fee} disabled/>
-                                </FormItem>  
-                            </Col>
-                            <Col span={6}>
-                                <FormItem label="Tracking Number">
-                                    <Input value={this.state.order.tracking_number} disabled/>
-                                </FormItem>  
-                            </Col>
-                        </Row>
+                            </div>
+                        </div>
                     </div>
                 </Form>
             </div>
