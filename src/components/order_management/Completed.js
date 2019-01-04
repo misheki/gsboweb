45253b5a3abd98f9437cb2bd6bbeff62b18bb8cc
alin <halimatun@one2pay.my>
2019-01-04 14:10:12 +0800

    edit UI completed order

--- src/components/order_management/Completed.js
+++ src/components/order_management/Completed.js
@@ -111,9 +111,9 @@ class Completed extends Component {
             )
         )
         return(
-            <div style={{padding:'30px'}}>
-                <Form layout="vertical" style={{backgroundColor:'white'}}> 
-                    <div style={{padding:'20px', marginBottom:'10px'}}>
+            <div style={{padding:'30px',}}>
+                <Form layout="vertical"> 
+                    <div style={{padding:'20px', marginBottom:'10px', backgroundColor:'white'}}>
                         <h2 style={{paddingBottom:'10px'}}>Order Ref. No. {this.state.order.order_ref_num}</h2>    
                         <Row gutter={8}>
                             <Col span={12}>
@@ -130,7 +130,6 @@ class Completed extends Component {
                                 <FormItem  {...formItemLayout} label="Tracking Number : " className="form-item">
                                     <p>{this.state.order.tracking_number} </p>
                                 </FormItem>
-
                             </Col>
                             <Col span={12}>
                                 <h3 style={{paddingBottom:'10px'}}>Customer Details </h3>  
@@ -140,9 +139,10 @@ class Completed extends Component {
                                 <p>{this.state.order.customer_contact_num}</p> 
                                 <p>{this.state.order.customer_email}</p> 
                             </Col>
-                        </Row>   
+                        </Row>
+                    </div>  
+                    <div style={{ padding:'20px', backgroundColor:'white'}}>
                         <h3 style={{paddingBottom:'10px'}}>Product Details</h3>
-                        <div style={{ backgroundColor: 'white', padding:'10px'}}>
                             <Row gutter={16} style={{ backgroundColor: '#e8e8e8', padding: '10px', paddingBottom: '0px', marginBottom: '10px' }}>
                                 <Col span={2}>
                                     <p>Item</p>
@@ -164,18 +164,17 @@ class Completed extends Component {
                                 </Col>
                             </Row>
                             {packageDetailItems}
-                             <div style={{float:'right', width:'30%'}}>
-                                <FormItem  labelCol={{ span: 12 }} wrapperCol={{ span: 12 }} label="Subtotal : "  className="form-item">
+                             {/* <div style={{float:'right', width:'23%'}}> */}
+                                <FormItem  labelCol={{ span: 20 }} wrapperCol={{ span: 3 }} label="Subtotal : "  className="form-item-right">
                                         <p>RM {this.state.order.order_total}</p> 
                                 </FormItem>
-                                <FormItem  labelCol={{ span: 12 }} wrapperCol={{ span: 12 }} label="Shipping Fee : "  className="form-item">
+                                <FormItem  labelCol={{ span: 20 }} wrapperCol={{ span: 3 }} label="Shipping Fee : "  className="form-item-right">
                                         <p>RM {this.state.order.shipping_fee} </p> 
                                 </FormItem>
-                                <FormItem  labelCol={{ span: 12 }} wrapperCol={{ span: 12 }} label="Total Amount : "  className="form-item">
+                                <FormItem  labelCol={{ span: 20 }} wrapperCol={{ span: 3 }} label="Total Amount : "  className="form-item-right">
                                         <p>RM {this.state.order.total}</p> 
                                 </FormItem>
-                            </div>
-                        </div>
+                            {/* </div> */}
                     </div>
                 </Form>
             </div>
