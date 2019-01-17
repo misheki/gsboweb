2d02498bf0a572e60af99450dca7601fc8290d17
Michelle Yee <michelle.yee@one2pay.my>
2019-01-17 11:59:09 +0800

    Print Order alignment and tweaks

--- src/components/order_management/order_management_components/PrintOrder.js
+++ src/components/order_management/order_management_components/PrintOrder.js
@@ -76,87 +76,73 @@ class PrintOrder extends Component {
                 };
             }));
         }, []);
-                                  
-        console.log(packageDetailItems);
 
         return (
             <PrintProvider>
                 <NoPrint>
                     <div style={{padding: '10px', paddingLeft: '30px', paddingRight: '30px', backgroundColor:' white' }}>
                         <Print>
-                            <div className="logo">
-                                <img src={logo} alt="logo" style={{ width: '15%' }} />
-                            </div>
-
                             <Form layout="vertical"> 
                                 <div style={{ padding: '20px', paddingBottom: '0px', fontSize: '12px' }}>
-                                    <h1 style={{ textAlign: "center" }}>ORDER CONFIRMATION</h1><hr></hr>
+                                    <img src={logo} alt="logo" style={{ width: '15%', marginRight: '110px' }} />
+                                    <span style={{fontSize: '25px', fontWeight: 'bold', color:'black'}}>ORDER CONFIRMATION</span>
+                                    <hr></hr>
                                     <h3 style={{ paddingBottom: '10px' }}>Order Ref. No. {order.order_ref_num}</h3>
 
-                                    <Row gutter={8}>
-                                        <Col span={10} style={{border: '1px solid black', padding: '10px'}}>
+                                    <Row gutter={16}>
+                                        <Col span={11} style={{border: '1px solid black', padding: '10px'}}>
                                             <h4 style={{paddingBottom:'10px'}}>Customer Details</h4>  
                                             <Row gutter={8}>
                                                 <Col span={8}>Name</Col>
                                                 <Col span={2}>:</Col>
-                                                <Col span={8}>{order.customer_name}</Col>
+                                                <Col span={14}>{order.customer_name}</Col>
                                             </Row>
                                             <Row gutter={8}>
                                                 <Col span={8}>Address</Col>
                                                 <Col span={2}>:</Col>
-                                                <Col span={8}>{order.customer_address ? order.customer_address : 'N/A'}</Col>
+                                                <Col span={14}>{order.customer_address ? order.customer_address : 'N/A'}</Col>
                                             </Row>
                                             {order.customer_postcode ? 
                                                 <Row gutter={8}>
                                                     <Col span={8}></Col>
                                                     <Col span={2}></Col>
-                                                    <Col span={8}> {order.customer_postcode ? order.customer_postcode + ', ' : null} {order.customer_state}</Col>
+                                                    <Col span={14}> {order.customer_postcode ? order.customer_postcode + ', ' : null} {order.customer_state}</Col>
                                                 </Row>
                                             : null}
                                             <Row gutter={8}>
                                                 <Col span={8}>Contact No.</Col>
                                                 <Col span={2}>:</Col>
-                                                <Col span={8}> {order.customer_contact_num ? order.customer_contact_num : 'N/A'}</Col>
+                                                <Col span={14}> {order.customer_contact_num ? order.customer_contact_num : 'N/A'}</Col>
                                             </Row>
                                             <Row gutter={8}>
                                                 <Col span={8}>Email</Col>
                                                 <Col span={2}>:</Col>
-                                                <Col span={8}>{order.customer_email ? order.customer_email : 'N/A'}</Col>
+                                                <Col span={14}>{order.customer_email ? order.customer_email : 'N/A'}</Col>
                                             </Row>
-                                            Attn: {order.customer_name}<br />
-                                            Address: {order.customer_address ? order.customer_address : 'N/A'}<br />
-                                            {order.customer_postcode ? order.customer_postcode + ', ' : null} {order.customer_state}<br />
-                                            Tel: {order.customer_contact_num ? order.customer_contact_num : 'N/A'} <br />
-                                            Email: {order.customer_email ? order.customer_email : 'N/A'}<br />
                                         </Col>
-                                        <Col span={1}>
-                                           
-                                        </Col>
-                                        <Col span={13} style={{border: '1px solid black', padding: '10px', fontSize: '10px'}}>
+     
+                                        <Col span={12} style={{border: '1px solid black', padding: '10px', fontSize: '12px', marginLeft: '5px'}}>
                                             <h4 style={{paddingBottom:'10px'}}>Order Details </h4>
                                             <Row gutter={8}>
-                                                <Col span={8}>Order ID</Col>
-                                                <Col span={2}>:</Col>
-                                                <Col span={8}>{order.order_ref_num}</Col>
+                                                <Col span={8}><b>Order ID</b></Col>
+                                                <Col span={2}><b>:</b></Col>
+                                                <Col span={14}><b>{order.order_ref_num}</b></Col>
                                             </Row>
                                             <Row gutter={8}>
                                                 <Col span={8}>Order Date</Col>
                                                 <Col span={2}>:</Col>
-                                                <Col span={8}>{order.order_date}</Col>
+                                                <Col span={14}>{order.order_date}</Col>
                                             </Row>
                                             <Row gutter={8}>
                                                 <Col span={8}>Shipping Method</Col>
                                                 <Col span={2}>:</Col>
-                                                <Col span={8}>{order.shipping_method ? order.shipping_method : 'Self Pickup'}</Col>
+                                                <Col span={14}>{order.shipping_method ? order.shipping_method : 'Self Pickup'}</Col>
                                             </Row>
                                             <Row gutter={8}>
                                                 <Col span={8}>Tracking No.</Col>
                                                 <Col span={2}>:</Col>
-                                                <Col span={8}>{order.tracking_number ? order.tracking_number : 'N/A'}</Col>
+                                                <Col span={14}>{order.tracking_number ? order.tracking_number : 'N/A'}</Col>
                                             </Row>
-                                            Order Date: {order.order_date}<br /> 
-                                            Shipping Method: {order.shipping_method ? order.shipping_method : 'Self Pickup'}<br />
-                                            {order.shipping_method ? <p>{'Tracking Number: ' + order.tracking_number}</p> : <br />}
                                         </Col>
                                        
                                     </Row>
@@ -170,36 +156,25 @@ class PrintOrder extends Component {
                                         pagination={false}
                                         dataSource={packageDetailItems}
                                         rowKey={item => packageDetailItems.id}>
-                                            <Column title="Item" dataIndex="id" key="id" className="printOrder" />
+                                            <Column title="No." dataIndex="id" key="id" className="printOrder" />
                                             <Column title="SKU" dataIndex="sku" key="sku" className="printOrder" />
                                             <Column title="Package" dataIndex="package_name" key="package_name" className="printOrder" />
                                             <Column title="Serial Number" dataIndex="serial_number" key="serial_number" className="printOrder" />
-                                            <Column title="Mobile Number" dataIndex="sim_card_number" key="sim_card_number" className="printOrder" />
-                                            <Column title="Unit Price (RM)" dataIndex="unit_price" key="unit_price" className="printOrder" />
+                                            <Column title="Unit Price" dataIndex="unit_price" key="unit_price" className="printOrder" render={(val) => 'RM ' + val} />
                                     </Table>
 
-                                    {/* <table>
-                                        <tr>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td></td>
-                                            <td>Subtotal:</td>
-                                            <td>RM {order.order_total}</td>
-                                        </tr>
-                                    </table> */}
-                                    <div style={{ paddingTop: '40px', fontSize: '12px' }} className="form-item-right">
-                                        <span className="font-bold">Subtotal: </span>RM {order.order_total}<br />
-                                        <span className="font-bold">Shipping: </span>RM {order.shipping_fee ? order.shipping_fee : '0.00'}<br />
-                                        <span className="font-bold">Less (Discount): </span> RM {order.discount ? order.discount : '0.00'}<br />
-                                        <span className="font-bold">Total Amount: </span>RM {order.total}<br />
+                                    <div style={{ paddingTop: '40px', fontSize: '12px', textAlign: 'right' }}>
+                                        <span style={{display: 'inline-block', width: '150px', fontWeight: 'bold', marginRight: '20px'}}>Subtotal : </span><span style={{textAlign: 'left', display: 'inline-block', width: '125px', fontWeight: 'bold'}}>RM {order.order_total}</span><br />
+                                        <span style={{display: 'inline-block', width: '150px', fontWeight: 'bold', marginRight: '20px'}}>Shipping : </span><span style={{textAlign: 'left', display: 'inline-block', width: '125px', fontWeight: 'bold'}}>RM {order.shipping_fee ? order.shipping_fee : '0.00'}</span><br />
+                                        <span style={{display: 'inline-block', width: '150px', fontWeight: 'bold', marginRight: '20px'}}>Less (Discount) : </span><span style={{textAlign: 'left', display: 'inline-block', width: '125px', fontWeight: 'bold'}}>RM {order.discount ? order.discount : '0.00'}</span><br />
+                                        <span style={{display: 'inline-block', width: '150px', fontWeight: 'bold', marginRight: '20px'}}>Total Amount : </span><span style={{textAlign: 'left', display: 'inline-block', width: '125px', fontWeight: 'bold'}}>RM {order.total}</span><br />
                                     </div>
 
                                     <div className="form-item-center" style={{ paddingTop: '40px', fontSize: '10px' }}>
                                         <span className="p-no-bottom">For assistance, you may reach out to our Customer Service via:</span><br />
                                         <span className="font-bold p-no-bottom">WhatsApp/SMS - +6016-339-9967</span><br />
                                         <span className="font-bold">Email - cs@globalsim.my</span><br />
-                                        <span className="p-top-40">This is a computer-generated document. The signature is not required.</span><br />
+                                        <span className="p-top-40">This is a computer-generated document. No signature is required.</span><br />
                                     </div>
                                 </div>
                             </Form>
